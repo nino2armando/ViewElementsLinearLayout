3d2f6b6201e071565756f1960e31431c90b58af9
nino2armando <nino2armando@yahoo.com>
2014-02-17 23:02:43 -0800

    minor changes

---
+++ app/src/main/java/com/example/app/services/UserDataContract.java
@@ -0,0 +1,24 @@
+package com.example.app.services;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Nino on 17/02/14.
+ */
+public final class UserDataContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public UserDataContract(){}
+
+    public static abstract class UserDataEntry implements BaseColumns{
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_FIRSTNAME = "firstname";
+        public static final String COLUMN_NAME_LASTNAME = "lastname";
+        public static final String COLUMN_NAME_AGE = "age";
+        public static final String COLUMN_IMAGE = "image";
+    }
+
+}
